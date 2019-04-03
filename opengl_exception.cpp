169b4228629b4c8b095d074890b2da7b6fc4dfc9
Paulo Ricardo <pauloricardojm@hotmail.com.br>
2019-04-03 10:16:20 -0300

    Exception class

---
+++ opengl_exception.cpp
@@ -0,0 +1,22 @@
+#include "inc/opengl_exception.hpp"
+
+OpenglException::OpenglException(const char *msg, unsigned int error_code)
+{
+    this->_msg_ = msg;
+    this->_error_code_ = error_code;
+}
+
+const char *OpenglException::get_message() const
+{
+    return this->_msg_;
+}
+
+unsigned int OpenglException::get_error_code() const
+{
+    return this->_error_code_;
+}
+
+OpenglException::~OpenglException()
+{
+
+}
