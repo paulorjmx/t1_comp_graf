86cff8eedf1685e78af965f946fa29f08b3f2fe4
Paulo Ricardo <pauloricardojm@hotmail.com.br>
2019-04-03 10:55:07 -0300

    Added support to OpenglException class

--- inc/shader.hpp
+++ inc/shader.hpp
@@ -15,8 +15,8 @@
                 bool is_created();
                 bool is_compiled();
                 void compile_shader();
-                int create_shader();
-                int set_shader_source(const char **source, unsigned int count);
+                void create_shader();
+                void set_shader_source(const char **source, unsigned int count);
                 void set_shader_type(GLenum shader_type);
                 unsigned int get_shader_id();
                 GLenum get_shader_type();
