179482086e69ccd47d800674d2668df469b8c5e1
Paulo Ricardo <pauloricardojm@hotmail.com.br>
2019-04-17 22:09:29 -0300

    Header of transformations

---
+++ inc/graphic_math.hpp
@@ -0,0 +1,17 @@
+#ifndef GRAPHIC_MATH
+    #define GRAPHIC_MATH
+        class GraphicMath
+        {
+            private:
+                float transf_matrix[16], angle;
+
+            public:
+                GraphicMath();
+                void translate_matrix(float x, float y, float z);
+                void rotate_matrix(float x, float y, float z, float velocity);
+                void stop_rotate();
+                float *get_transf_matrix();
+                ~GraphicMath();
+        };
+
+#endif
