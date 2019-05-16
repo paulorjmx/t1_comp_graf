4eb35c732a35f5ed2fd4a57fde70469879221a97
Paulo Ricardo Jordao Miranda <pauloricardojm@hotmail.com.br>
2019-05-16 16:16:49 -0300

    Fix the index of vertexes

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 #include "../inc/opengl_exception.hpp"
 #include "../inc/graphic_math.hpp"
 #include "../inc/vertex3d.hpp"
+#include <vector>
 #include <cmath>
 #include <GLFW/glfw3.h>
 
@@ -25,112 +26,101 @@ int main(int argc, char const *argv[])
 {
     ShaderSource vertex_source; // ShaderSource é a classe criada com o intuito de conter código de shaders.
     ShaderSource fragment_source;
-    float vertices[36] = {0.0f, 0.0f, 0.0f,
-                 		0.25f, 0.0f, 0.0f,
-                 		0.25f, 0.25f, 0.0f,
-
-                 		0.0f, 0.0f, 0.0f,
-                 		0.0f, 0.25f, 0.0f,
-                 		-0.25f, 0.25f, 0.0f,
-
-                 		0.0f, 0.0f, 0.0f,
-                 		-0.25f, 0.0f, 0.0f,
-                 		-0.25f, -0.25f, 0.0f,
-
-                 		0.0f, 0.0f, 0.0f,
-                 		0.0f, -0.25f, 0.0f,
-                 		0.25f, -0.25f, 0.0f,
-    };
-    try
-    {
-        vertex_source.set_shader_source_from_file("src/v_shader.shd"); // Pega o código do vertex shader
-        fragment_source.set_shader_source_from_file("src/f_shader.shd"); // Pega o código do fragment shader
-
-
-        glfwInit();
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-        GLFWwindow *window = glfwCreateWindow(800, 600, "Trabalho 1 - Computação Gráfica", NULL, NULL);
-        if(window == NULL)
-        {
-            cout << "Failed to create window!" << endl;
-            glfwTerminate();
-            return -1;
-        }
-        glfwMakeContextCurrent(window);
-        if(!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress))
-        {
-            cout << "Failed to link functions" << endl;
-            glfwTerminate();
-            return -1;
-        }
-
-        Shader vertex_shader(GL_VERTEX_SHADER);
-        Shader fragment_shader(GL_FRAGMENT_SHADER);
-        ShaderProgram shader_program;
-
-        vertex_shader.create_shader();
-        fragment_shader.create_shader();
-        vertex_shader.set_shader_source(vertex_source.get_shader_source(), 1);
-        fragment_shader.set_shader_source(fragment_source.get_shader_source(), 1);
-        vertex_shader.compile_shader();
-        fragment_shader.compile_shader();
-
-        shader_program.create_shader_program();
-        shader_program.attach_shader(vertex_shader);
-        shader_program.attach_shader(fragment_shader);
-        shader_program.link_program();
-
-
-        VertexBuffer vb(1); // Cria 1 VertexBuffer
-        VertexArray va(1); // Cria 1 VertexArray
-
-        va.gen_buffer();
-        vb.gen_buffer();
-
-        va.bind_vertex_array(0);
-
-        vb.bind_buffer(GL_ARRAY_BUFFER, 0);
-        vb.buffer_data(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *) 0);
-        glEnableVertexAttribArray(0);
-
-        glfwSetFramebufferSizeCallback(window, framebuffer_resize_callback);
-        glfwSetKeyCallback(window, key_pressed_callback); // Verifica se a tecla foi pressionada (utilizada para a barra de espaço)
-        while(!glfwWindowShouldClose(window))
-        {
-            process_input(window); // Processa a tecla pressionada, ou segurada, pelo usuário.
-            glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-            glClear(GL_COLOR_BUFFER_BIT);
-
-            shader_program.use_program();
-            if(ROTATE_FLAG == 0x01) // Checa se o botao para rodar foi ativado
-            {
-                printf("%f\n", VELOCITY);
-                matrix.rotate_matrix(0.0f, 0.0f, 0.0f, VELOCITY);
-            }
-            else
-            {
-                matrix.stop_rotate();
-            }
-
-            unsigned int transform_location = glGetUniformLocation(shader_program.get_shader_program_id(), "transform");
-            glUniformMatrix4fv(transform_location, 1, GL_FALSE, matrix.get_transf_matrix());
-            va.bind_vertex_array(0);
-            glDrawArrays(GL_TRIANGLES, 0, 12);
-
-            glfwSwapBuffers(window);
-            glfwPollEvents();
-        }
-
-        glfwTerminate();
-    }
-    catch(OpenglException &e)
-    {
-        cout << "Error " << e.get_error_code() << ": " << e.get_message() << endl;
-        return -1;
-    }
+    vector<Vertex3D> vertices;
+    vector<Vertex3D> normals;
+    vector<Vertex3D> uv;
+    load_obj("assets/cube/cube.obj", vertices, uv, normals);
+    cout << vertices.size() << endl;
+    // try
+    // {
+    //     vertex_source.set_shader_source_from_file("src/v_shader.shd"); // Pega o código do vertex shader
+    //     fragment_source.set_shader_source_from_file("src/f_shader.shd"); // Pega o código do fragment shader
+    //
+    //
+    //     glfwInit();
+    //     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    //     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    //     GLFWwindow *window = glfwCreateWindow(800, 600, "Trabalho 1 - Computação Gráfica", NULL, NULL);
+    //     if(window == NULL)
+    //     {
+    //         cout << "Failed to create window!" << endl;
+    //         glfwTerminate();
+    //         return -1;
+    //     }
+    //     glfwMakeContextCurrent(window);
+    //     if(!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress))
+    //     {
+    //         cout << "Failed to link functions" << endl;
+    //         glfwTerminate();
+    //         return -1;
+    //     }
+    //
+    //     Shader vertex_shader(GL_VERTEX_SHADER);
+    //     Shader fragment_shader(GL_FRAGMENT_SHADER);
+    //     ShaderProgram shader_program;
+    //
+    //     vertex_shader.create_shader();
+    //     fragment_shader.create_shader();
+    //     vertex_shader.set_shader_source(vertex_source.get_shader_source(), 1);
+    //     fragment_shader.set_shader_source(fragment_source.get_shader_source(), 1);
+    //     vertex_shader.compile_shader();
+    //     fragment_shader.compile_shader();
+    //
+    //     shader_program.create_shader_program();
+    //     shader_program.attach_shader(vertex_shader);
+    //     shader_program.attach_shader(fragment_shader);
+    //     shader_program.link_program();
+    //
+    //
+    //     VertexBuffer vb(1); // Cria 1 VertexBuffer
+    //     VertexArray va(1); // Cria 1 VertexArray
+    //
+    //     va.gen_buffer();
+    //     vb.gen_buffer();
+    //
+    //     va.bind_vertex_array(0);
+    //
+    //     vb.bind_buffer(GL_ARRAY_BUFFER, 0);
+    //     vb.buffer_data(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex3D), &vertices[0], GL_STATIC_DRAW);
+    //
+    //     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *) 0);
+    //     glEnableVertexAttribArray(0);
+    //
+    //     glfwSetFramebufferSizeCallback(window, framebuffer_resize_callback);
+    //     glfwSetKeyCallback(window, key_pressed_callback); // Verifica se a tecla foi pressionada (utilizada para a barra de espaço)
+    //     while(!glfwWindowShouldClose(window))
+    //     {
+    //         process_input(window); // Processa a tecla pressionada, ou segurada, pelo usuário.
+    //         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    //         glClear(GL_COLOR_BUFFER_BIT);
+    //
+    //         shader_program.use_program();
+    //         if(ROTATE_FLAG == 0x01) // Checa se o botao para rodar foi ativado
+    //         {
+    //             printf("%f\n", VELOCITY);
+    //             matrix.rotate_matrix(0.0f, 0.0f, 0.0f, VELOCITY);
+    //         }
+    //         else
+    //         {
+    //             matrix.stop_rotate();
+    //         }
+    //
+    //         unsigned int transform_location = glGetUniformLocation(shader_program.get_shader_program_id(), "transform");
+    //         glUniformMatrix4fv(transform_location, 1, GL_FALSE, matrix.get_transf_matrix());
+    //         va.bind_vertex_array(0);
+    //         glDrawArrays(GL_TRIANGLES, 0, 12);
+    //
+    //         glfwSwapBuffers(window);
+    //         glfwPollEvents();
+    //     }
+    //
+    //     glfwTerminate();
+    // }
+    // catch(OpenglException &e)
+    // {
+    //     cout << "Error " << e.get_error_code() << ": " << e.get_message() << endl;
+    //     return -1;
+    // }
     return 0;
 }
 
@@ -204,6 +194,7 @@ void load_obj(const char *file_name, vector<Vertex3D> &vertexes, vector<Vertex3D
         while(1)
         {
             model_file.getline(line_readed, sizeof(line_readed));
+            cout << line_readed << endl;
             if(model_file.eof())
             {
                 break;
@@ -213,7 +204,7 @@ void load_obj(const char *file_name, vector<Vertex3D> &vertexes, vector<Vertex3D
                 if(line_readed[0] == 'v') // If the line begins with v (vertices)
                 {
                     token = &line_readed[2];
-                    sscanf(token, " %f %f %f", &x, &y, &z);
+                    sscanf(token, "%f %f %f", &x, &y, &z);
                     temp_vertices.push_back(Vertex3D(x, y, z));
                 }
                 else if(line_readed[0] == 'v' && line_readed[1] == 'n') // If the line begins with vn (normals)
@@ -222,7 +213,7 @@ void load_obj(const char *file_name, vector<Vertex3D> &vertexes, vector<Vertex3D
                     sscanf(token, "%f %f %f", &x, &y, &z);
                     temp_normals.push_back(Vertex3D(x, y, z));
                 }
-                else if(line_readed[0] == 'v' && line_readed[1] == 'n') // If the line begins with vt (texture coordinate)
+                else if(line_readed[0] == 'v' && line_readed[1] == 't') // If the line begins with vt (texture coordinate)
                 {
                     token = &line_readed[3];
                     sscanf(token, "%f %f", &x, &y);
@@ -231,7 +222,8 @@ void load_obj(const char *file_name, vector<Vertex3D> &vertexes, vector<Vertex3D
                 else if(line_readed[0] == 'f')
                 {
                     token = &line_readed[2];
-                    sscanf(token, "%d/%d/%d %d/%d/%d %d/%d/%d", &vertex_index[0], &uv_index[0], &normal_index[0],
+                    sscanf(token, "%ld/%ld/%ld %ld/%ld/%ld %ld/%ld/%ld",
+                                                                &vertex_index[0], &uv_index[0], &normal_index[0],
                                                                 &vertex_index[1], &uv_index[1], &normal_index[1],
                                                                 &vertex_index[2], &uv_index[2], &normal_index[2]);
                     vertices_indexes.push_back(vertex_index[0]);
@@ -249,19 +241,19 @@ void load_obj(const char *file_name, vector<Vertex3D> &vertexes, vector<Vertex3D
 
         for(int i = 0; i < vertices_indexes.size(); i++)
         {
-            Vertex3D v = temp_vertices[vertices_indexes[i]];
+            Vertex3D v = temp_vertices[vertices_indexes[i] - 1];
             vertexes.push_back(v);
         }
 
         for(int i = 0; i < normals_indexes.size(); i++)
         {
-            Vertex3D n = temp_normals[normals_indexes[i]];
+            Vertex3D n = temp_normals[normals_indexes[i] - 1];
             normals.push_back(n);
         }
 
         for(int i = 0; i < uv_indexes.size(); i++)
         {
-            Vertex3D _uv = temp_uv[uv_indexes[i]];
+            Vertex3D _uv = temp_uv[uv_indexes[i] - 1];
             uv.push_back(_uv);
         }
     }
