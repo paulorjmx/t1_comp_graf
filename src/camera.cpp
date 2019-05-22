73f330fb8467c500a59fba3a80fbad60900945d0
Paulo Ricardo J. Miranda <pauloricardojm@hotmail.com.br>
2019-05-22 10:58:16 -0300

    Fixed constructor

--- src/camera.cpp
+++ src/camera.cpp
@@ -4,38 +4,40 @@
 
 Camera::Camera(Point ref_point, Point look_at)
 {
-    float *zview_coordinates, *u_coordinates, *v_coordinates, *ref_point_coordinates;
-    Vec3D zview(ref_point, look_at);
-    zview.normalize();
-    zview_coordinates = zview.get_coodinates();
+    float *forward, *up, *right, *ref_point_coordinates;
+    Vec3D n(ref_point, look_at);
 
-    Vec3D u = zview.cross_product(Vec3D(Point(0.0f, 0.0f, 0.0f), Point(0.0f, 1.0f, 0.0f)));
+    Vec3D tmp(Point(0.0f, 0.0f, 0.0f), Point(0.0f, 1.0f, 0.0f));
+    n.normalize();
+
+    Vec3D u = tmp.cross_product(n);
     u.normalize();
-    u_coordinates = u.get_coodinates();
 
-    Vec3D v = zview.cross_product(u);
-    v_coordinates = v.get_coodinates();
+    Vec3D v = n.cross_product(u);
 
+    forward = n.get_coodinates();
+    right = u.get_coodinates();
+    up = v.get_coodinates();
     ref_point_coordinates = ref_point.get_point();
 
-    this->view_mat[0] = u_coordinates[0];
-    this->view_mat[1] = v_coordinates[0];
-    this->view_mat[2] = zview_coordinates[0];
+    this->view_mat[0] = right[0];
+    this->view_mat[1] = right[1];
+    this->view_mat[2] = right[2];
     this->view_mat[3] = 0.0f;
 
-    this->view_mat[4] = u_coordinates[1];
-    this->view_mat[5] = v_coordinates[1];
-    this->view_mat[6] = zview_coordinates[1];
+    this->view_mat[4] = up[0];
+    this->view_mat[5] = up[1];
+    this->view_mat[6] = up[2];
     this->view_mat[7] = 0.0f;
 
-    this->view_mat[8] = u_coordinates[2];
-    this->view_mat[9] = v_coordinates[2];
-    this->view_mat[10] = zview_coordinates[2];
+    this->view_mat[8] = forward[0];
+    this->view_mat[9] = forward[1];
+    this->view_mat[10] = forward[2];
     this->view_mat[11] = 0.0f;
 
-    this->view_mat[12] = ((-1.0f * ref_point_coordinates[0]) * u_coordinates[0]) + ((-1.0f * ref_point_coordinates[1]) * u_coordinates[1]) + ((-1.0f * ref_point_coordinates[2]) * u_coordinates[2]);
-    this->view_mat[13] = ((-1.0f * ref_point_coordinates[0]) * v_coordinates[0]) + ((-1.0f * ref_point_coordinates[1]) * v_coordinates[1]) + ((-1.0f * ref_point_coordinates[2]) * v_coordinates[2]);
-    this->view_mat[14] = ((-1.0f * ref_point_coordinates[0]) * zview_coordinates[0]) + ((-1.0f * ref_point_coordinates[1]) * zview_coordinates[1]) + ((-1.0f * ref_point_coordinates[2]) * zview_coordinates[2]);
+    this->view_mat[12] = ref_point_coordinates[0];
+    this->view_mat[13] = ref_point_coordinates[1];
+    this->view_mat[14] = ref_point_coordinates[2];
     this->view_mat[15] = 1.0f;
 
 }
