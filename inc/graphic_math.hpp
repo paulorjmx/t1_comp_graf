1de2b0cd9f3870793d172a966a01593c6c9a6a9a
Paulo Ricardo Jordao Miranda <pauloricardojm@hotmail.com.br>
2019-06-15 14:52:27 -0300

    Added width height in persp function

--- inc/graphic_math.hpp
+++ inc/graphic_math.hpp
@@ -10,7 +10,7 @@
                 void translate_matrix(float x, float y, float z);
                 void scale_matrix(float x, float y, float z);
                 void rotate_matrix(float x, float y, float z, float velocity);
-                void perspective_matrix(float fov, float aspect, float znear);
+                void perspective_matrix(float fov, float width, float height, float aspect, float znear);
                 void stop_rotate();
                 float *get_transf_matrix();
                 ~GraphicMath();
